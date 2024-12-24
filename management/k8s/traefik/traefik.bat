66a849404b16dae4b0a857195efe78cf74049683
suidori <leo7750@naver.com>
2024-12-24 17:42:46 +0900

    1224 ingress

---
+++ management/k8s/traefik/traefik.bat
@@ -0,0 +1,13 @@
+@echo off
+
+kubectl create namespace traefik
+kubectl apply -f 00-account.yml
+kubectl apply -f 00-role.yml
+kubectl apply -f 01-role-binding.yml
+kubectl apply -f 02-traefik.yml
+kubectl apply -f 02-traefik-config.yml
+kubectl apply -f 02-traefik-services.yml
+kubectl apply -f 03-traefik-ingress.yml
+
+echo All resources have been applied successfully.
+pause
