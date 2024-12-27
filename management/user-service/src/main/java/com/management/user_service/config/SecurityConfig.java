15ceba1ca08da3aff8e18b1dfcc6ba888259dc2e
DESKTOP-SCJ83VA\user <parkmyounghoi@gmail.com>
2024-12-27 17:28:15 +0900

    241227

--- management/user-service/src/main/java/com/management/user_service/config/SecurityConfig.java
+++ management/user-service/src/main/java/com/management/user_service/config/SecurityConfig.java
@@ -1,16 +1,37 @@
 package com.management.user_service.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig {
+
+    private final List<String> allowedOrigins;
+    private final Environment environment;
+
+    public SecurityConfig(Environment environment) {
+        this.environment = environment;
+        String[] origins = environment.getProperty("cors.allowed-origins", String[].class);
+        if (origins != null) {
+            allowedOrigins = List.of(origins);
+            log.error(allowedOrigins.toString());
+        } else {
+            allowedOrigins = List.of(); // 기본값으로 빈 리스트
+        }
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -27,6 +48,15 @@ public class SecurityConfig {
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/**").permitAll()
                 .anyRequest().authenticated()
         );
+
+        http.cors(cors -> cors.configurationSource(request -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(allowedOrigins);
+            configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            configuration.setAllowedHeaders(List.of("*"));
+            configuration.setAllowCredentials(true);
+            return configuration;
+        }));
 
         return http.build();
     }
